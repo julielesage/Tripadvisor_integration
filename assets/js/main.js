07b0ac066792464b1eda1ab199db6372ca051e9d
jlesage <57409607+julielesage@users.noreply.github.com>
2020-02-02 21:55:44 +0100

    Add files via upload

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,25 +5,34 @@ document.addEventListener("DOMContentLoaded", () => {
   console.log("document chargé!");
 
   /* DARK MODE */
-  document.querySelector("#dark-mode").addEventListener("click", event => {
-    event.preventDefault();
+  /*document.querySelector("#dark-mode").addEventListener("click", event => {
+    /*event.preventDefault();
     /* if (!darkMode) {
           $.querySelector("body").classList.add("dark-mode");
         } else {
           $.querySelector("body").classList.remove("dark-mode");
         }
-        darkMode = !darkMode; */
+        darkMode = !darkMode; 
     $.querySelector("body").classList.toggle("dark-mode");
-  });
+  });*/
   document.querySelector("#btn-register").addEventListener("click", () => {
+    /*Si l'on veut annuler le comportement par défaut d'une balise HTML au clic de souris, il faut utiliser la méthode preventDefault() */
     event.preventDefault();
+    console.log("btn selectionne");
     document.querySelector(".modal").classList.toggle("hidden");
   });
 });
 
+const switchModal = () => {
+  console.log("switchModal");
+  document.querySelector(".modal").classList.toggle("hidden");
+};
+
 window.addEventListener("scroll", () => {
   console.log(window.scrollY);
-  if (scrollY > 60) {
-    document.querySelector("sub-menu").classList.toggle("hidden");
+  if (window.scrollY > 252) {
+    document.querySelector(".sub-menu").classList.remove("hidden");
   }
+  else 
+    document.querySelector(".sub-menu").classList.add("hidden");
 });
